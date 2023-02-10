b19242c12f3ce70a187291ec39b2ac5934a11be7
id251 <danyalider178@gmail.com>
2023-02-10 18:30:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder word= new StringBuilder();
+        int []num={1,2,3,4,5};
+        System.out.println(word.append(Arrays.toString(num)));
+
+
+    }
+}
