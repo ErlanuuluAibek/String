4d220850f31589121e9d0493c4692a582f090dff
id251 <danyalider178@gmail.com>
2023-02-10 20:19:10 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
+import java.io.*;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        StringBuilder word= new StringBuilder();
-        int []num={1,2,3,4,5};
-        System.out.println(word.append(Arrays.toString(num)));
-
-
+    public static void main(String[] args) throws IOException {
+        Song song=new Song();
+        FileWriter fileWriter=new FileWriter("Song.txt");
+        fileWriter.write("XXTENTATION\n");
+        fileWriter.write("Moonlight\n");
+        fileWriter.write("Spotlight, uh, moonlight, uh Spotlight, uh");
+        fileWriter.close();
+        FileReader fileReader=new FileReader("Song.txt");
+        Scanner scanner=new Scanner(fileReader);
+        while(scanner.hasNextLine()){
+            song.setAuthor(scanner.nextLine());
+            song.setTitle(scanner.nextLine());
+            song.setText(scanner.nextLine());
+        }
+        fileReader.close();
+        System.out.println(song);
     }
 }
